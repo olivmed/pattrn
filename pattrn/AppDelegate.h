08fcd452dfbb767dd5cbef97e593e99155c7acd8
olivmed <olivier.medina1@gmail.com>
2016-03-11 02:23:17 +0100

    first

---
+++ pattrn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pattrn
+//
+//  Created by Olivier Medina on 11/03/2016.
+//  Copyright © 2016 Olivier Medina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pattrn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pattrn
+//
+//  Created by Olivier Medina on 11/03/2016.
+//  Copyright © 2016 Olivier Medina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
